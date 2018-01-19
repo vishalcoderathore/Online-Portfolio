ad3cfb1d6ffc0018d9667a7660e63773c54ff276
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-19 12:23:18 -0600

    Added Smooth Scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 class Header extends React.Component {
+
+    scrollToSection = (self) => {
+        // get the target href
+        var href = $(self).attr('href');
+        // get the target position
+        var targetPos = typeof $(href).offset() !== "undefined" ? $(href).offset().top : 0;
+
+        console.log(targetPos);
+        // scroll to target
+        $('html, body').animate({
+            scrollTop: targetPos
+        }, 400);
+    };
+
+    scrollWindow = (event) => {
+        this.scrollToSection(event.target);
+        event.preventDefault();
+    };
+
     render() {
         return (
             <nav id="navbar" className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top py-4" >
@@ -9,7 +28,7 @@ class Header extends React.Component {
                     <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNavbar"><span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavbar">
-                        <ul className="navbar-nav ml-auto">
+                        <ul className="navbar-nav ml-auto" onClick={this.scrollWindow}>
                             <li className="nav-item" id="Home">
                                 <a href="#aboutMe" className="nav-link active">Home</a>
                             </li>
@@ -17,13 +36,13 @@ class Header extends React.Component {
                                 <a href="#edu" className="nav-link">Education</a>
                             </li>
                             <li className="nav-item" id="Work">
-                                <a href="#" className="nav-link">Work</a>
+                                <a href="myWork" className="nav-link">Work</a>
                             </li>
                             <li className="nav-item" id="Projects">
-                                <a href="#" className="nav-link">Projects</a>
+                                <a href="myProjects" className="nav-link">Projects</a>
                             </li>
                             <li className="nav-item" id="Contact">
-                                <a href="#" className="nav-link">Contact</a>
+                                <a href="myContact" className="nav-link">Contact</a>
                             </li>
                         </ul>
                     </div>

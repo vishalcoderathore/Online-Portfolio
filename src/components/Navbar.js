4db7afac1c542715553ec4ca47b7768e504d744d
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-24 01:19:59 -0600

    Fixed button collapse bug on mobile screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,29 +19,35 @@ class Header extends React.Component {
         event.preventDefault();
     };
 
+    //Close Navbar Button on mobile devices 
+    closeNavbarButton = (e) => {
+        console.log('mobile');
+            $(".navbar-collapse").collapse('hide');       
+    };
+
     render() {
         return (
             <nav id="navbar" className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top py-4" >
                 <div className="container">
-                    <a href="#" className="navbar-brand"><span><i className="fa fa-user-o" aria-hidden="true"></i></span> Portfolio</a>
-                    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNavbar"><span className="navbar-toggler-icon"></span>
+                    <a href="#aboutMe" className="navbar-brand" onClick={this.scrollWindow}><span><i className="fa fa-user-o" aria-hidden="true"></i></span> Vishal Singh Rathore</a>
+                    <button id="collapseButton" className="navbar-toggler" data-toggle="collapse" data-target="#navbarNavbar"><span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavbar">
                         <ul className="navbar-nav ml-auto" onClick={this.scrollWindow}>
                             <li className="nav-item" id="Home">
-                                <a href="#aboutMe" className="nav-link active">Home</a>
+                                <a href="#aboutMe" className="nav-link active" onClick={this.closeNavbarButton}>Home</a>
                             </li>
                             <li className="nav-item" id="Education">
-                                <a href="#edu" className="nav-link">Education</a>
+                                <a href="#edu" className="nav-link" onClick={this.closeNavbarButton}>Education</a>
                             </li>
                             <li className="nav-item" id="Work">
-                                <a href="#myWork" className="nav-link">Work</a>
+                                <a href="#myWork" className="nav-link" onClick={this.closeNavbarButton}>Work</a>
                             </li>
                             <li className="nav-item" id="Projects">
-                                <a href="#myProjects" className="nav-link">Projects</a>
+                                <a href="#myProjects" className="nav-link" onClick={this.closeNavbarButton}>Projects</a>
                             </li>
                             <li className="nav-item" id="Contact">
-                                <a href="#myContact" className="nav-link">Contact</a>
+                                <a href="#myContact" className="nav-link" onClick={this.closeNavbarButton}>Contact</a>
                             </li>
                         </ul>
                     </div>

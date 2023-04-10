8af54c3c806dc768720f57009e721ffcb5654653
Vishal <vishal.rathore@multitech.com>
2023-04-10 10:37:23 -0500

    React auto changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b6abf26617374d8"; }
+/******/ 		__webpack_require__.h = function() { return "27174e357c073e2b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

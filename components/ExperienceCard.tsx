89505210300610182b4e2ae4c1c43d712cfcbf26
Vishal Rathore <vishal.rathore@multitech.com>
2023-04-17 16:23:00 -0500

    updated work exp to add dynamic exp cards using resources, adjusted card css properties

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { motion } from "framer-motion";
+import _ from "lodash";
 
-type Props = {};
+interface ChildProps {
+  workId: string;
+  organization: string;
+  position: string;
+  duration: string;
+  location: string;
+  description: string;
+  companyLogo: string;
+  tech: string[];
+}
 
-function ExperienceCard({}: Props) {
+function ExperienceCard(props: ChildProps) {
   return (
     <article
       className='flex 
       flex-col 
       rounded-lg 
       items-center 
-      space-y-7 
+      space-y-0 
       flex-shrink-0 
-      sm:w-[500px] 
+      xs:w-[300px]
+      sm:w-[600px] 
       md:w-[600px] 
       xl:w-[900px] 
-      snap-center 
+      snap-center
       bg-[#292929] 
       p-10 
       sm:hover:opacity-100 
@@ -41,51 +52,34 @@ function ExperienceCard({}: Props) {
         viewport={{
           once: true,
         }}
-        className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
-        src='/images/multitech.png'
+        className='w-32 h-32 xl:w-[200px] xl:h-[200px] object-scale-down object-center'
+        src={props.companyLogo}
         alt=''
       />
 
-      <div className='px-0 sm:px-10'>
+      <div className='xs:px-1 sm:px-10'>
         <h4 className='text-xl lg:text-2xl font-semibold text-gray-500 mt-1'>
-          Software Engineer, Cloud Platform
+          {props.position}
         </h4>
-        <p className='font-bold my-1'>Multi-Tech Systems, Inc.</p>
+        <p className='font-bold my-1'>{props.organization}</p>
         <div className='flex space-x-2 my-2'>
           {/* Tech Used */}
-          <img
-            className='h-10 w-10 rounded-full'
-            src='https://www.osano.com/hs-fs/hubfs/logos/vuejs.png?width=199&height=199&name=vuejs.png'
-            alt=''
-          />
-          {/* Tech Used */}
-          <img
-            className='h-10 w-10 rounded-full'
-            src='https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/112012/rails.png?itok=UeYwdITD'
-            alt=''
-          />
-          {/* Tech Used */}
-          <img
-            className='h-10 w-10 rounded-full'
-            src='https://devoncroft.com/wp-content/uploads/2021/01/AWS-Logo-e1610483403406.png'
-            alt=''
-          />
-          {/* Tech Used */}
-
-          <img
-            className='h-10 w-10 rounded-full'
-            src='https://g2si.fr/forma_ico/linux.png'
-            alt=''
-          />
+          {_.map(props.tech, (item, index) => {
+            return (
+              <img
+                className='h-10 w-10 rounded-full'
+                src={item}
+                alt=''
+                key={"experienceCardTechImage" + index}
+              />
+            );
+          })}
         </div>
-        <p>July 2019 - Present</p>
-        <ul className='list-disc space-y-4 ml-5 text-sm md:text-base text-justify'>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
-        </ul>
+        <p className='text-base text-gray-500'>{props.duration}</p>
+
+        <p className='text-xs sm:text-base text-left mt-1'>
+          {props.description}
+        </p>
       </div>
     </article>
   );
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 import ExperienceCard from "./ExperienceCard";
+import _ from "lodash";
+import { work } from "../resources/workSources";
 
 type Props = {};
 
-function WorkExperience({}: Props) {
+const WorkExperience = ({}: Props) => {
   return (
     <motion.div
       initial={{
@@ -20,14 +22,45 @@ function WorkExperience({}: Props) {
       <h1 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-xl sm:text-2xl '>
         Experience
       </h1>
+      <h3 className='absolute top-36 uppercase xs:text-xs sm:text-base xs:tracking-[2px] sm:tracking-[3px] text-gray-500 px-5'>
+        Seasoned professional with expertise in full-stack web development
+        across diverse industries
+      </h3>
 
       <div className='w-full flex space-x-10 overflow-x-scroll px-10 pb-10 pt-36 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#d42d2d]/80'>
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+        {_.map(
+          work,
+          (
+            {
+              workId,
+              organization,
+              position,
+              duration,
+              location,
+              description,
+              companyLogo,
+              tech,
+            },
+            index
+          ) => {
+            return (
+              <ExperienceCard
+                workId={workId}
+                organization={organization}
+                position={position}
+                duration={duration}
+                location={location}
+                description={description}
+                companyLogo={companyLogo}
+                tech={tech}
+                key={"experienceCard" + index}
+              />
+            );
+          }
+        )}
       </div>
     </motion.div>
   );
-}
+};
 
 export default WorkExperience;
